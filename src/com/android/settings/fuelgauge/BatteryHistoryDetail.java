b583a81e6a5dfb59a1748d04a9fc3e5a3d61f186
Vairavan Srinivasan <vairav@codeaurora.org>
2011-01-20 21:17:12 -0800

    BatteryHistoryDetail to use batteryinfo service for stats
    Change-Id: I8c60df74399b0a1552c8127953cd900ebb00ca1d

--- src/com/android/settings/fuelgauge/BatteryHistoryDetail.java
+++ src/com/android/settings/fuelgauge/BatteryHistoryDetail.java
@@ -20,18 +20,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Parcel;
 
+import com.android.internal.app.IBatteryStats;
+import android.util.Log;
+import android.os.RemoteException;
 import com.android.internal.os.BatteryStatsImpl;
+import android.os.ServiceManager;
 import com.android.settings.R;
 
 public class BatteryHistoryDetail extends Activity {
     public static final String EXTRA_STATS = "stats";
 
+    private static final String TAG = "BatteryHistoryDetail";
+
+    IBatteryStats mBatteryInfo;
     private BatteryStatsImpl mStats;
 
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+
         byte[] data = getIntent().getByteArrayExtra(EXTRA_STATS);
+
+        if (data == null) {
+
+             mBatteryInfo = IBatteryStats.Stub.asInterface(
+                     ServiceManager.getService("batteryinfo"));
+
+             if (mBatteryInfo != null) {
+                 try {
+                     data = mBatteryInfo.getStatistics();
+                 } catch(RemoteException e) {
+                     Log.e(TAG, "RemoteException - fetch of stats failed");
+                 }
+             }
+        }
+
         Parcel parcel = Parcel.obtain();
         parcel.unmarshall(data, 0, data.length);
         parcel.setDataPosition(0);
