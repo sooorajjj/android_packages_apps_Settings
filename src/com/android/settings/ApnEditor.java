7f7a1d4589a50d2b1daba35213018b840d51ddad
Alex Yakavenka <ayakav@codeaurora.org>
2012-06-18 14:02:26 -0700

    Telephony: Use new apn.sim.operator_numeric property
        Use new property to filter apns. We need this new property
        because icc.operator_numeric property is also used in cdma
        and ApnSetting UI might display wrong set of apns if the
        peroperty value used was set by RuimRecords.
    
    Change-Id: I019deb6b5de3c910f215170f9c9d7b9501577430
    CRs-Fixed: 362846

--- src/com/android/settings/ApnEditor.java
+++ src/com/android/settings/ApnEditor.java
@@ -220,7 +220,7 @@ public class ApnEditor extends PreferenceActivity
         mCursor = managedQuery(mUri, sProjection, null, null);
         mCursor.moveToFirst();
 
-        fillUi();
+        fillUi(intent.getStringExtra(ApnSettings.OPERATOR_NUMERIC_EXTRA));
     }
 
     @Override
@@ -237,7 +237,7 @@ public class ApnEditor extends PreferenceActivity
         super.onPause();
     }
 
-    private void fillUi() {
+    private void fillUi(String defaultOperatorNumeric) {
         if (mFirstTime) {
             mFirstTime = false;
             // Fill in all the values from the db in both text editor and summary
@@ -255,14 +255,12 @@ public class ApnEditor extends PreferenceActivity
             mMnc.setText(mCursor.getString(MNC_INDEX));
             mApnType.setText(mCursor.getString(TYPE_INDEX));
             if (mNewApn) {
-                String numeric =
-                    SystemProperties.get(TelephonyProperties.PROPERTY_ICC_OPERATOR_NUMERIC);
                 // MCC is first 3 chars and then in 2 - 3 chars of MNC
-                if (numeric != null && numeric.length() > 4) {
+                if (defaultOperatorNumeric != null && defaultOperatorNumeric.length() > 4) {
                     // Country code
-                    String mcc = numeric.substring(0, 3);
+                    String mcc = defaultOperatorNumeric.substring(0, 3);
                     // Network code
-                    String mnc = numeric.substring(3);
+                    String mnc = defaultOperatorNumeric.substring(3);
                     // Auto populate MNC and MCC for new entries, based on what SIM reports
                     mMcc.setText(mcc);
                     mMnc.setText(mnc);
