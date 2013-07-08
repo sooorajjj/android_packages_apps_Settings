369e605a0c3bb1e632dce9fd051ccf920c8a5722
kaiyiz <kaiyiz@codeaurora.org>
2013-07-08 14:58:35 +0800

    Setting: Only getAveragePower returns 0 not to show Battery use age data
    In Power_profile.xml define the min value is 0.1, and getAveragePower
    results is 0.1 which compares with 10, so it will always not to show Battery
    use age data.
    
    Modify the judgment conditions, that only getAveragePower results is 0 not to
    show Battery use age data.
    
    CRs-Fixed: 508927
    
    Change-Id: Ib1d601148b16d5126a52cf573468bbe62d56fd2a

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -394,7 +394,7 @@ public class PowerUsageSummary extends PreferenceFragment implements Runnable {
         hist.setOrder(-1);
         mAppListGroup.addPreference(hist);
         
-        if (mPowerProfile.getAveragePower(PowerProfile.POWER_SCREEN_FULL) < 10) {
+        if (mPowerProfile.getAveragePower(PowerProfile.POWER_SCREEN_FULL) == 0) {
             addNotAvailableMessage();
             return;
         }
