da61296b75bc40dfa2c9ac6b8d115cfac82226e9
Juffin Alex Varghese <jalex@codeaurora.org>
2013-08-12 12:08:40 +0530

    Bluetooth: Revert "Bluetooth: Remove out of range devices after ScanComplete"
    Revert https://review-android.quicinc.com/#/c/112289/
    
    CRs-Fixed: 519649
    Change-Id: I48c86ab0463f17acedcd74bebe4b2c575e67e197

--- src/com/android/settings/bluetooth_msm/CachedBluetoothDeviceManager.java
+++ src/com/android/settings/bluetooth_msm/CachedBluetoothDeviceManager.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2013 The Linux Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -125,17 +126,13 @@ final class CachedBluetoothDeviceManager {
 
 
     public synchronized void onScanningStateChanged(boolean started) {
+        if (!started) return;
+
         // If starting a new scan, clear old visibility
         // Iterate in reverse order since devices may be removed.
         for (int i = mCachedDevices.size() - 1; i >= 0; i--) {
             CachedBluetoothDevice cachedDevice = mCachedDevices.get(i);
-            if (started) {
-                cachedDevice.setVisible(false);
-            } else if (!started &&
-                cachedDevice.getBondState() == BluetoothDevice.BOND_NONE &&
-                cachedDevice.isRemovable()) {
-                mCachedDevices.remove(cachedDevice);
-            }
+            cachedDevice.setVisible(false);
         }
     }
 
@@ -150,15 +147,6 @@ final class CachedBluetoothDeviceManager {
         CachedBluetoothDevice cachedDevice = findDevice(device);
         if (cachedDevice != null) {
             cachedDevice.onUuidChanged();
-        }
-    }
-
-    public synchronized void onDeviceDeleted(CachedBluetoothDevice cachedDevice) {
-        Log.d(TAG,"onDeviceDeleted");
-        if (cachedDevice != null &&
-            cachedDevice.getBondState() == BluetoothDevice.BOND_NONE &&
-            cachedDevice.isRemovable()) {
-            mCachedDevices.remove(cachedDevice);
         }
     }
 
