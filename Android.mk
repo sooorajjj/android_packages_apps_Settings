248a07fb10e768af7134f4318445b0f078ec6f5b
jianzhou <jianzhou@codeaurora.org>
2013-09-04 18:58:11 +0800

    Settings: Add IZat location feature
    Add "IZat location" item in "Location access" page in
    Settings. The feature can be turned on and off with a checkbox.
    
    Change-Id: I1876c025783c2fa4b7ae14e61161114c4090d693

--- Android.mk
+++ Android.mk
@@ -10,7 +10,9 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)  \
   src/com/android/cabl/ICABLService.aidl
 
 LOCAL_SRC_FILES += \
-  src/com/android/display/IPPService.aidl
+  src/com/android/display/IPPService.aidl \
+  src/com/android/location/XT/IXTSrv.aidl \
+  src/com/android/location/XT/IXTSrvCb.aidl
 
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
