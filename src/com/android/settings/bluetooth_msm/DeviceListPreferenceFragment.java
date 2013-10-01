1d21884409a80f4d943a7412a9e0ab2da98deb49
Anantha Krishnan <ananthk@codeaurora.org>
2013-10-01 12:44:29 +0530

    Revert "Bluetooth: Revert "Bluetooth: Remove out of range devices after ScanComplete""
    This reverts commit da61296b75bc40dfa2c9ac6b8d115cfac82226e9.

--- src/com/android/settings/bluetooth_msm/DeviceListPreferenceFragment.java
+++ src/com/android/settings/bluetooth_msm/DeviceListPreferenceFragment.java
@@ -129,6 +129,23 @@ public abstract class DeviceListPreferenceFragment extends
         }
     }
 
+    void removeOorDevices() {
+        Collection<CachedBluetoothDevice> cachedDevices =
+                mLocalManager.getCachedDeviceManager().getCachedDevicesCopy();
+        for (CachedBluetoothDevice cachedDevice : cachedDevices) {
+             if (cachedDevice.getBondState() == BluetoothDevice.BOND_NONE &&
+                 !cachedDevice.isVisible()) {
+                 Log.d(TAG, "Device Removed " + cachedDevice);
+                 BluetoothDevicePreference preference = mDevicePreferenceMap.get(cachedDevice);
+                 if (preference != null) {
+                     mDeviceListGroup.removePreference(preference);
+                 }
+                 mDevicePreferenceMap.remove(cachedDevice);
+                 cachedDevice.setRemovable(true);
+             }
+         }
+    }
+
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
             Preference preference) {
@@ -190,6 +207,9 @@ public abstract class DeviceListPreferenceFragment extends
     }
 
     public void onScanningStateChanged(boolean started) {
+        if (started == false) {
+          removeOorDevices();
+        }
         updateProgressUi(started);
     }
 
