3d3b7cc5f71ef412d6eba69eb8cfe6d512bf05a2
Srinu Jella <sjella@codeaurora.org>
2014-06-03 16:47:50 +0530

    Bluetooth: Reset the isPairing flag on BT state changed to OFF
    Reset the isPairing flag on BT state changed to OFF, so that
    it doesn't get stuck in pairing if the BT turn OFF was
    initiated from non settings APP(Airplane mode, Power widget).
    
    Change-Id: Ie05fe3d1b6362349f468fee04ba8dc97d0d55096
    CRs-Fixed: 674549

--- src/com/android/settings/bluetooth/LocalBluetoothAdapter.java
+++ src/com/android/settings/bluetooth/LocalBluetoothAdapter.java
@@ -183,6 +183,9 @@ public final class LocalBluetoothAdapter {
                 mProfileManager.setBluetoothStateOn();
             }
         }
+        else if (state == BluetoothAdapter.STATE_OFF) {
+            isPairing = false;
+        }
     }
 
     // Returns true if the state changed; false otherwise.
