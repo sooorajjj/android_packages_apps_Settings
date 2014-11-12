9d5b480a85b05b809590aa268c0f74ce4cb247e3
Sanket Padawe <sanketpadawe@google.com>
2014-11-12 12:41:35 -0800

    UI changes for icons in sim settings
    + UI changes relate to displaying sim icons in sim settings
    + Correcting way to display phone number inside sim settings dialog
    
    Bug: 18356419
    Change-Id: I1fe025fac52f20b607d4492b99cfef9588ec9416

--- src/com/android/settings/sim/SimSettings.java
+++ src/com/android/settings/sim/SimSettings.java
@@ -28,6 +28,7 @@ import android.content.SharedPreferences.Editor;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -603,12 +604,14 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
     private class SimPreference extends Preference{
         private SubscriptionInfo mSubscriptionInfo;
         private int mSlotId;
-        private int[] colorArr;
+        private int[] tintArr;
+        Context mContext;
 
         public SimPreference(Context context, SubscriptionInfo subInfoRecord, int slotId) {
             super(context);
 
             mSubscriptionInfo = subInfoRecord;
+            mContext = context;
             mSlotId = slotId;
             setKey("sim" + mSlotId);
             update();
@@ -625,6 +628,7 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
                         mSubscriptionInfo.getDisplayName(),
                         mSubscriptionInfo.getNumber()));
                 setEnabled(true);
+                setIcon(new BitmapDrawable(res, (mSubInfoRecord.createIconBitmap(mContext))));
             } else {
                 setSummary(R.string.sim_slot_empty);
                 setFragment(null);
