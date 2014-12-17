d74f40da0b7786e23387994ceef2ce58f1370bf8
Nagaraj <nanna@codeaurora.org>
2014-12-17 12:14:05 -0800

    Settings: Add support for STA+P2PGO+SAP Concurrency
    1. Add menu to create Start/Stop P2PGO from WifiP2pSettings.
    2. Avoid WLAN driver load/unload during SAP start.
    3. Avoid WLAN driver load/unload during STA start.
    
    Change-Id: I20891339e953a5a1c2617907a41b5698ae6a1db0
    CRs-Fixed: 741484

--- src/com/android/settings/wifi/WifiEnabler.java
+++ src/com/android/settings/wifi/WifiEnabler.java
@@ -90,7 +90,7 @@ public class WifiEnabler implements SwitchBar.OnSwitchChangeListener  {
         mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 
         mIntentFilter = new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION);
-        // The order matters! We really should not depend on this. :(
+        // The order matters! We really should not depend on this.
         mIntentFilter.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
         mIntentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
 
@@ -207,7 +207,9 @@ public class WifiEnabler implements SwitchBar.OnSwitchChangeListener  {
         int wifiApState = mWifiManager.getWifiApState();
         if (isChecked && ((wifiApState == WifiManager.WIFI_AP_STATE_ENABLING) ||
                 (wifiApState == WifiManager.WIFI_AP_STATE_ENABLED))) {
-            mWifiManager.setWifiApEnabled(null, false);
+            if (!mWifiManager.getConcurrency()) {
+                mWifiManager.setWifiApEnabled(null, false);
+            }
         }
 
         if (!mWifiManager.setWifiEnabled(isChecked)) {
