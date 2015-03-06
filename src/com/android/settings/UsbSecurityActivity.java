ddf7e2035748a3ff8c5b754f1e47530012f01013
kaiyiz <kaiyiz@codeaurora.org>
2015-03-06 12:01:20 +0800

    Settings: Requirement for Hardware Interface
    First time plug in USB cable to DUT, it needs to insert
    SIM/USIM card to DUT to activate USB port. After this
    operation, DUT is available without SIM/USIM card.
    After factory resetting/flashing/build upgrading, USB port
    will be turned off again. Inserting SIM/USIM card can active
    DUT USB port again. In case of USB port is not activated, if
    end user plug in USB cable, DUT should remind user on UI
    that only SIM/USIM card inserting first can activate USB
    port, and description to user that locking operation is
    to avoid third party flashing app or code evilly after DUT
    send out from factory.
    
    SIM/USIM card: Not mandatory to use CMCC SIM/USIM card.
    Card did not recognized to be active or arrears.
    
    USB port limitation: Limiting debug/storage/USB HOST mode.
    Does not limit charge mode.
    
    Change-Id: Ib34d729d340601f185f1edbb6e52b9e32ea0505d
    CRs-Fixed: 803421

---
+++ src/com/android/settings/UsbSecurityActivity.java
@@ -0,0 +1,102 @@
+/*
+Copyright (c) 2015, The Linux Foundation. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package com.android.settings;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.android.internal.app.AlertActivity;
+import com.android.internal.app.AlertController;
+import com.android.internal.telephony.IccCardConstants;
+import com.android.internal.telephony.TelephonyIntents;
+
+public class UsbSecurityActivity extends AlertActivity {
+
+    private static final String TAG = "UsbSecurity";
+    private IntentFilter mFilter;
+    private BroadcastReceiver mReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final AlertController.AlertParams mParams = mAlertParams;
+        LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mParams.mView = vi.inflate(R.layout.usb_security_dialog, null);
+        mParams.mViewSpacingSpecified = true;
+        mParams.mViewSpacingLeft = 15;
+        mParams.mViewSpacingRight = 15;
+        mParams.mViewSpacingTop = 5;
+        mParams.mViewSpacingBottom = 5;
+        mParams.mPositiveButtonText = getString(android.R.string.yes);
+        mParams.mPositiveButtonListener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        };
+        setupAlert();
+        mFilter = new IntentFilter();
+        mFilter.addAction(TelephonyIntents.ACTION_SIM_STATE_CHANGED);
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (TelephonyIntents.ACTION_SIM_STATE_CHANGED.equals(intent.getAction())) {
+                    String stateExtra = intent
+                            .getStringExtra(IccCardConstants.INTENT_KEY_ICC_STATE);
+                    if (IccCardConstants.INTENT_VALUE_ICC_READY.equals(stateExtra)
+                            || IccCardConstants.INTENT_VALUE_ICC_IMSI.equals(stateExtra)
+                            || IccCardConstants.INTENT_VALUE_ICC_LOADED.equals(stateExtra)) {
+                        finish();
+                    }
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+}
