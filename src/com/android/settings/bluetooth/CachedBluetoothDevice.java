a6078ee9b11773a940669dc484754876a74ab8d1
Balraj Selvaraj <bselva@codeaurora.org>
2015-04-08 20:57:50 +0530

    Bluetooth: GAP: Revert "Bluetooth: Fix for device gets stuck in pairing"
    This reverts commit f4c10bb6c4c7bdb73e0bd52fd12f39cefeffd634.
    
    - Reverted the change since L settings app design is taking care
      of simultaneous pairing or pair to two devices one after the other.
    
    CRs-Fixed: 820146
    Change-Id: I76b897b1ba5ff13428cbdc2d785a29d4d6342426

--- src/com/android/settings/bluetooth/CachedBluetoothDevice.java
+++ src/com/android/settings/bluetooth/CachedBluetoothDevice.java
@@ -301,28 +301,15 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
     }
 
     boolean startPairing() {
-        boolean pairingState = mLocalAdapter.checkPairingState();
-
-        if (pairingState == true)
-        {
-            Log.v(TAG, "Pairing is ongoing");
-            return true;
-        }
-
-        mLocalAdapter.setPairingState(true);
-        Log.v(TAG, "startPairing : isPairing : " + pairingState);
-
         // Pairing is unreliable while scanning, so cancel discovery
         if (mLocalAdapter.isDiscovering()) {
             mLocalAdapter.cancelDiscovery();
         }
 
         if (!mDevice.createBond()) {
-            mLocalAdapter.setPairingState(false);
             return false;
         }
 
-        Log.v(TAG, "startPairing CreateBond : isPairing : " + pairingState);
         mConnectAfterPairing = true;  // auto-connect after pairing
         return true;
     }
@@ -614,9 +601,7 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
     }
 
     void onBondingStateChanged(int bondState) {
-        boolean pairingState = mLocalAdapter.checkPairingState();
         if (bondState == BluetoothDevice.BOND_NONE) {
-            mLocalAdapter.setPairingState(false);
             mProfiles.clear();
             mDevice.setAlias(null);
             mConnectAfterPairing = false;  // cancel auto-connect
@@ -624,20 +609,17 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
             setMessagePermissionChoice(ACCESS_UNKNOWN);
             mMessageRejectionCount = 0;
             saveMessageRejectionCount();
-            Log.v(TAG,"BondState none: isPairing : " + pairingState);
         }
 
         refresh();
 
         if (bondState == BluetoothDevice.BOND_BONDED) {
-            mLocalAdapter.setPairingState(false);
             if (mDevice.isBluetoothDock()) {
                 onBondingDockConnect();
             } else if (mConnectAfterPairing) {
                 connect(false);
             }
             mConnectAfterPairing = false;
-            Log.v(TAG,"BondState bonded: isPairing : " + pairingState);
         }
     }
 
