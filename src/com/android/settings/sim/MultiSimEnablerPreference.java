43d9b3f6b17c46a20ab546a149d5ef686096794c
wangjing <wangjing@codeaurora.org>
2015-04-10 11:19:51 +0800

    Settings: Deactivate SIM card can be done from UI.
    When deactivate SIM card from UI, the boolean mCurrentState will be
    set to false. Then it will deactivate SIM card accorging to the
    value of mCurrentState. But the value of mCurrentState can be
    changed by mStateChanegReceiver and others.
    
    Replace the boolean mCurrentState by a new boolean mRequest. Then
    deactivate SIM card can be done from UI according to mRequest.
    
    CRs-Fixed: 818197
    Change-Id: Iab95381c30ea87c6bbda24a1d7d4046d29e56695

--- src/com/android/settings/sim/MultiSimEnablerPreference.java
+++ src/com/android/settings/sim/MultiSimEnablerPreference.java
@@ -90,6 +90,7 @@ public class MultiSimEnablerPreference extends Preference implements OnCheckedCh
     private int mSlotId;
     private SubscriptionInfo mSir;
     private boolean mCurrentState;
+    private boolean mRequest;
 
     private boolean mCmdInProgress = false;
     private int mSwitchVisibility = View.VISIBLE;
@@ -259,7 +260,7 @@ public class MultiSimEnablerPreference extends Preference implements OnCheckedCh
     }
 
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        mCurrentState = isChecked;
+        mRequest = isChecked;
         logd("onClick: " + isChecked);
 
         synchronized (mSyncLock) {
@@ -284,7 +285,7 @@ public class MultiSimEnablerPreference extends Preference implements OnCheckedCh
             }
         }
 
-        if (!mCurrentState) {
+        if (!mRequest) {
             if (getActivatedSubInfoCount(mContext) > 1) {
                 logd("More than one sub is active, Deactivation possible.");
                 showAlertDialog(CONFIRM_ALERT_DLG_ID, 0);
@@ -308,7 +309,7 @@ public class MultiSimEnablerPreference extends Preference implements OnCheckedCh
 
         showProgressDialog();
         setEnabled(false);
-        if (mCurrentState) {
+        if (mRequest) {
             SubscriptionManager.activateSubId(mSir.getSubscriptionId());
         } else {
             SubscriptionManager.deactivateSubId(mSir.getSubscriptionId());
@@ -362,7 +363,7 @@ public class MultiSimEnablerPreference extends Preference implements OnCheckedCh
     private void showProgressDialog() {
         String title = mSir == null ? "SUB" : mSir.getDisplayName().toString();
 
-        String msg = mContext.getString(mCurrentState ? R.string.sim_enabler_enabling
+        String msg = mContext.getString(mRequest ? R.string.sim_enabler_enabling
                 : R.string.sim_enabler_disabling);
         dismissDialog(sProgressDialog);
         sProgressDialog = new ProgressDialog(mContext);
