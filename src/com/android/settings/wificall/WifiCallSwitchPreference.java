fab02f251d6c783684ab4c7ee5629d992dc26101
baiwei1218 <baiwei1218@thundersoft.com>
2015-05-06 16:33:23 +0800

    Settings:Wi-Fi Calling menu
    Add a Wi-Fi Calling switch menu in the WirelessSettings interface,
    to some satisfy specific carrier's requirement.
    
    Change-Id: I73f4f8e4469ac9d3392109974b4d9fa3c1c71e4e
    (cherry-picked from 2771fdac5002177d72645a2fa229a9e39c83ba95)

---
+++ src/com/android/settings/wificall/WifiCallSwitchPreference.java
@@ -0,0 +1,111 @@
+/*
+     Copyright (c) 2015, The Linux Foundation. All Rights Reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package com.android.settings.wificall;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.preference.SwitchPreference;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Checkable;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class WifiCallSwitchPreference extends SwitchPreference {
+
+    private static final String TAG = "WifiCallSwitchPreference";
+
+    private boolean mCheckedStatus;
+
+    public WifiCallSwitchPreference(Context context) {
+        super(context);
+    }
+
+    public WifiCallSwitchPreference(Context context, AttributeSet attrs,
+            int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public WifiCallSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public WifiCallSwitchPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, com.android.internal.R.attr.switchPreferenceStyle);
+    }
+
+    @Override
+    protected void onClick() {
+        super.onClick();
+        Log.i(TAG, "onClik mCheckedStatus : " + isChecked());
+        // The switchpreference turn on/off must keep as the same as the interal switch
+        setChecked(!isChecked());
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+
+        View checkableView = view
+                .findViewById(com.android.internal.R.id.switchWidget);
+        if (checkableView != null && checkableView instanceof Checkable) {
+            if (checkableView instanceof Switch) {
+                Log.i(TAG, "start setOnCheckedChangeListener");
+                ((Switch) checkableView).setClickable(true);
+                final Switch switchView = (Switch) checkableView;
+                // Add the switch checkedChangeListener for that
+                // when user press the switch to turn on/off the wifi calling
+                // press the text part for the wifi calling preference interface.
+                switchView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(
+                            CompoundButton buttonView, boolean isChecked) {
+                        Log.i(TAG, "start onCheckedChanged isChecked : " + isChecked);
+                        if (!callChangeListener(isChecked)) {
+                            buttonView.setChecked(!isChecked);
+                            return;
+                        }
+                        WifiCallSwitchPreference.this.setChecked(isChecked);
+                        onSwitchClicked();
+                    }
+                });
+            }
+        }
+    }
+
+    private void onSwitchClicked(){
+        // FIX ME : when the wifi-calling switch is turn/on, do something
+        Log.d(TAG, "onSwitchClicked "+isChecked());
+    }
+
+}
