1137237792a78f8042a58e73b839dc19b3318203
baiwei1218 <baiwei1218@thundersoft.com>
2015-06-19 13:48:47 +0800

    Setting: Fix bug cause by base upgrade
    There is duplicate variable when base upgrade,
    so change code avoid some problem.
    
    Change-Id: I33d717dc2ce93a73bed22b45742dce14aae2960e

--- src/com/android/settings/WirelessSettings.java
+++ src/com/android/settings/WirelessSettings.java
@@ -83,6 +83,7 @@ public class WirelessSettings extends SettingsPreferenceFragment
     private static final String KEY_TOGGLE_NSD = "toggle_nsd"; //network service discovery
     private static final String KEY_CELL_BROADCAST_SETTINGS = "cell_broadcast_settings";
     private static final String KEY_WIFI_CALLING_SETTINGS = "wifi_calling_settings";
+    private static final String KEY_WIFI_CALL_SETTINGS = "wifi_call_settings";
 
     public static final String EXIT_ECM_RESULT = "exit_ecm_result";
     public static final int REQUEST_CODE_EXIT_ECM = 1;
@@ -485,9 +486,9 @@ public class WirelessSettings extends SettingsPreferenceFragment
             Resources res = getResources();
             if (!res.getBoolean(
                     com.android.internal.R.bool.config_regional_wifi_calling_menu_enable)) {
+                removePreference(KEY_WIFI_CALL_SETTINGS);
+            } else if(findPreference(KEY_WIFI_CALL_SETTINGS) != null) {
                 removePreference(KEY_WIFI_CALLING_SETTINGS);
-            } else if(findPreference("wifi_calling_settings") != null) {
-                removePreference("wifi_calling_settings");
             }
         }
     }
@@ -497,7 +498,7 @@ public class WirelessSettings extends SettingsPreferenceFragment
         super.onStart();
 
         initSmsApplicationSetting();
-        Preference wifiCall = findPreference("wifi_call_settings");
+        Preference wifiCall = findPreference(KEY_WIFI_CALL_SETTINGS);
         if (wifiCall != null && wifiCall instanceof WifiCallSwitchPreference) {
             ((WifiCallSwitchPreference)wifiCall).getWifiCallingPreference();
         }
